b7b53471664438019fc29e2d0b0c9199b8edaac4
repair-cat <simf61@yandex.ru>
2023-06-07 11:35:03 +0400

    items have added

--- warehouse/api/models.py
+++ warehouse/api/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+
 class Item(models.Model):
     name = models.CharField(max_length=50)
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    qty = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ('name',)
+        db_table = 'items'
+
+    def __str__(self):
+        return self.name
