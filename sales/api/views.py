2281b8301f75fd56b8aedf24089964ea3db06927
repair-cat <simf61@yandex.ru>
2023-06-09 11:58:32 +0400

    order's views

---
+++ sales/api/views.py
@@ -0,0 +1,85 @@
+from rest_framework import viewsets
+from food.models import Recipe
+from .models import *
+from .serializers import OrderListSerializer, OrderDetailSerializer, OrderItemSerializer
+from .permissions import CustomerOrderOrReadOnly
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAuthenticated
+
+User = get_user_model()
+
+
+# ---------------------------------------------For Nested Routers--------------------------------------------
+class OrderViewSet(viewsets.ModelViewSet):
+    """
+    Создан для вложенных маршрутов, связанных с Заказом
+    Все методы для работы с заказами.
+    Доступ: создание заказа для любого аутентифицированного юзера
+            действия над заказами доступны для владельцев заказа или суперпользователю
+    """
+
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [CustomerOrderOrReadOnly]
+        return [permission() for permission in permission_classes]
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Order.objects.all()
+        else:
+            return Order.objects.filter(customer=self.request.user.id)
+
+    def get_serializer_class(self):
+        """Выбор сериализатора в зависимости от применяемого метода"""
+        if self.action == 'list':
+            return OrderListSerializer
+        return OrderDetailSerializer
+
+    def perform_create(self, serializer):
+        """При создании заказа текущий юзер заносится в поле Customer"""
+        items = self.request.data.pop('items')
+
+        order = Order.objects.create(
+            customer=self.request.user,
+            pay_method=self.request.data.get('pay_method'),
+        )
+
+        for item_data in items:
+            recipe = Item.objects.filter(title=item_data.get('item')).first()
+            OrderItem.objects.create(
+                recipe=item,
+                qty=item_data.get('qty'),
+                order=order
+            )
+
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    """
+    Создан для вложенных маршрутов, связанных с Заказом
+    Для взаимодействия с рецептами заказа
+    get, post, put, patch, delete
+    """
+    serializer_class = OrderItemSerializer
+
+    def get_queryset(self):
+        return OrderRecipe.objects.filter(order=self.kwargs['orders_pk'])
+# ----------------------------------------------------------------------------------------------------------
+
+
+class OrderRecipeView(viewsets.ModelViewSet):
+    """
+    Конечная точка API, позволяющая просматривать, создавать или редактировать продукты заказа.
+    get, post, put, patch, delete
+    сортировка по заказам
+    доступ: Ко всем рецептам заказов в БД имеет доступ Суперпользователь
+            Любой пользователь имеет доступ к своим рецептам заказа.
+    """
+    queryset = OrderRecipe.objects.order_by('order')
+    serializer_class = OrderItemSerializer
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return self.queryset
+        return self.queryset.filter(order__customer=self.request.user)
