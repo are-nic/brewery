da709e04a69ae13288b96e4f2a2de5cb5e62aa87
repair-cat <simf61@yandex.ru>
2023-06-09 11:57:26 +0400

    permissions were changed

--- warehouse/api/views.py
+++ warehouse/api/views.py
@@ -1,13 +1,19 @@
 from rest_framework import viewsets
 from .serializers import ItemSerializer
 from .models import Item
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 
 class ItemViewSet(viewsets.ModelViewSet):
     """
-    All methods are allow for Admins
+    GET method is available for Authenticated users. All methods are available for Admins.
     """
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-    permission_classes = [IsAdminUser]
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
