8ec562be7cd173d58d1cd04a194f5da069582582
repair-cat <simf61@yandex.ru>
2023-06-11 07:08:25 +0400

    item was added

--- sales/api/models.py
+++ sales/api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 User = settings.AUTH_USER_MODEL
 
@@ -24,9 +26,22 @@ class Order(models.Model):
         return self.customer.name
 
 
+class Item(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    qty = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ('name',)
+        db_table = 'items'
+
+    def __str__(self):
+        return self.name
+
+
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, related_name='recipes', on_delete=models.CASCADE)
-    item = models.CharField()
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.SET_NULL)
     qty = models.PositiveIntegerField(default=0)
 
     class Meta:
@@ -34,4 +49,11 @@ class OrderItem(models.Model):
         db_table = 'order_item'
 
     def __str__(self):
-        return self.item
+        return self.item.name
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    """ create a token when user was created """
+    if created:
+        Token.objects.create(user=instance)
