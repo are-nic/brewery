5a1d81026ff70ea6e2c5bcd5b142648ef61aa790
repair-cat <simf61@yandex.ru>
2023-06-12 17:40:42 +0400

    models changed

--- sales/api/models.py
+++ sales/api/models.py
@@ -7,25 +7,6 @@ from rest_framework.authtoken.models import Token
 User = settings.AUTH_USER_MODEL
 
 
-class Order(models.Model):
-    PAY_METHOD = [
-        ('UPON_RECEIPT', 'Pay upon receipt'),
-        ('ONLINE', 'Pay online'),
-    ]
-    customer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    pay_method = models.CharField(max_length=50, choices=PAY_METHOD)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    is_paid = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = ('updated_at',)
-        db_table = 'order'
-
-    def __str__(self):
-        return self.customer.name
-
-
 class Item(models.Model):
     name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
@@ -37,6 +18,19 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('updated_at',)
+        db_table = 'order'
+
+    def __str__(self):
+        return f'Order from {self.customer.username}'
 
 
 class OrderItem(models.Model):
