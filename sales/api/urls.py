e02658b633a7074a0028a75fe35a990eb7c9ade1
repair-cat <simf61@yandex.ru>
2023-06-12 17:41:02 +0400

    removed unusable routers

--- sales/api/urls.py
+++ sales/api/urls.py
@@ -1,23 +1,16 @@
 from rest_framework.routers import SimpleRouter
-from .views import OrderViewSet, OrderItemViewSet, RegisterUserView
+from .views import OrderViewSet,  RegisterUserView, ItemViewSet
 from rest_framework.authtoken import views
 from django.urls import path
-from rest_framework_nested import routers
 
 router = SimpleRouter(trailing_slash=False)
 
-# вложенные маршруты к заказам и рецептам заказа
-# /order - all current user's Orders
-# /orders/{id} - show a order's detail via id
 router.register('orders', OrderViewSet, basename='order')
-# /orders/{orders_pk}/item - all one order's items
-orders_router = routers.NestedSimpleRouter(router, 'orders', lookup='order')
-# /order/{order_pk}/item/{item_pk} - детали рецепта по id рецепта
-orders_router.register('items', OrderItemViewSet, basename='item')
+
+router.register('items', ItemViewSet, basename='items')
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('token-auth/', views.obtain_auth_token)
 ]
 urlpatterns += router.urls
-urlpatterns += orders_router.urls
