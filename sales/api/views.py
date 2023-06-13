10ec328217d32d58ec112047d5ce1451b1391cfe
repair-cat <simf61@yandex.ru>
2023-06-13 17:09:24 +0400

    create and update methods were overrode

--- sales/api/views.py
+++ sales/api/views.py
@@ -59,6 +59,86 @@ class OrderViewSet(viewsets.ModelViewSet):
                 order=order
             )
 
+    def create(self, request, *args, **kwargs):
+        """ Override method fom POST order """
+        items_data = request.data.get('items')
+        order_items = []
+        if items_data:
+            for item_data in items_data:                        # перебираем все товары заказа из тела запроса
+                item_id = item_data.get('item')                 # получаем id Товара, который хотим заказать
+                order_item_qty = item_data.get('qty')           # получаем кол-во, которое необходимо заказать
+                max_qty = item_data.get('max_qty')              # получаем значение поля "Максимальное кол-во"
+
+                if item_id:                                     # если переданы в теле запроса id товара со склада
+                    if order_item_qty and order_item_qty > 0:   # если передано в теле запроса кол-во товара и оно больше 0
+                        try:
+                            item = Item.objects.get(pk=item_id)     # получаем экземпляр Товара
+                            if item.qty >= order_item_qty:          # если кол-во запрашиваемого товара меньше либо равно кол-ву Товара на складе
+                                order_items.append(OrderItem(item=item, qty=order_item_qty))
+                                item.qty -= order_item_qty          # уменьшаем кол-во Товара на складе
+                                item.save()                         # сохраняем изменения в Товаре
+                            else:
+                                return Response({'error': f"Not enough quantity available for item: {item.name}"}, status=status.HTTP_400_BAD_REQUEST)
+                        except Item.DoesNotExist:
+                            return Response({'error': f"Item with id {item_id} does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+
+                    elif max_qty:                                   # если в теле запроса передам параметр "max_qty" и он True
+                        try:
+                            item = Item.objects.get(pk=item_id)     # получаем экземпляр Товара
+                            if item.qty > 0:                        # если кол-во запрашиваемого товара больше 0
+                                order_items.append(OrderItem(item=item, qty=item.qty))
+                                item.qty = 0                        # уменьшаем кол-во Товара на складе до 0
+                                item.save()                         # сохраняем изменения в Товаре
+                            else:
+                                return Response({'error': f"Not enough quantity available for item: {item.name}"}, status=status.HTTP_400_BAD_REQUEST)
+                        except Item.DoesNotExist:
+                            return Response({'error': f"Item with id {item_id} does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+
+            if len(order_items) > 0:                                        # если в списке Товаров заказа есть экземпляры
+                order = Order.objects.create(customer=self.request.user)    # создаем экземпляр заказа
+                for order_item in order_items:                              # перебираем все Товары заказа из списка
+                    order_item.order = order                                # присваиваем им заказ, к которому они принадлежат
+                    order_item.save()                                       # сохраняем Товары заказа в БД
+            else:
+                return Response({'error': "Not enough data to create Order"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'Order complete - order_id': order.id}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'error': "No items provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+
+        items_data = request.data.get('items')                              # Получить данные из запроса
+        print(items_data)
+        order_items = []
+
+        if items_data:
+            for item_data in items_data:                                    # перебираем все товары заказа из тела запроса
+                item_id = item_data.get('item')                             # получаем id Товара, который хотим заказать
+                max_qty = item_data.get('max_qty')                          # получаем значение поля "Максимальное кол-во"
+
+                if max_qty:                                                         # Если поле 'max_qty' равно True, выполнить логику создания заказа
+                    order = Order.objects.create(customer=self.request.user)        # Создать заказ
+                    try:
+                        item = Item.objects.get(pk=item_id)                         # получаем экземпляр Товара
+                        if item.qty > 0:                                            # если кол-во запрашиваемого товара больше 0
+                            order_items.append(OrderItem(item=item, qty=item.qty))
+                            item.qty = 0                                            # уменьшаем кол-во Товара на складе до 0
+                            item.save()                                             # сохраняем изменения в Товаре
+                        else:
+                            return Response({'error': f"Not enough quantity available for item: {item.name}"}, status=status.HTTP_400_BAD_REQUEST)
+                    except Item.DoesNotExist:
+                        return Response({'error': f"Item with id {item_id} does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+                    item = OrderItem.objects.create(order=order, **item_data)
+
+                    # Сериализовать и вернуть созданный заказ
+                    serializer = self.get_serializer(order)
+                    return Response(serializer.data)
+
+                # Если поле 'max_qty' не равно True, выполнить стандартное обновление
+                return super().update(request, *args, **kwargs)
+
+        return Response({'error': "No items provided"}, status=status.HTTP_400_BAD_REQUEST)
+
 
 # ----------------------------------------- ITEM -----------------------------------
 class ItemViewSet(mixins.ListModelMixin,
