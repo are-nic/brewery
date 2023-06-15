c7090f5229355ec669459c40a72ba784a6b8341e
repair-cat <simf61@yandex.ru>
2023-06-15 12:23:38 +0400

    route for order_items was changed

--- accounting/api/urls.py
+++ accounting/api/urls.py
@@ -1,10 +1,10 @@
 from rest_framework.routers import SimpleRouter
-from .views import SalaryViewSet
+from .views import OrderItemViewSet
 from rest_framework.authtoken import views
 from django.urls import path
 
 router = SimpleRouter(trailing_slash=False)
-router.register('salary', SalaryViewSet, basename='salary')
+router.register('order_items', OrderItemViewSet, basename='order_item')
 
 urlpatterns = [
     path('token-auth/', views.obtain_auth_token)
