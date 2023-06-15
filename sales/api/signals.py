67e7303db57382454991097435131e36a99f6400
repair-cat <simf61@yandex.ru>
2023-06-15 12:22:56 +0400

    signals for sales projects

---
+++ sales/api/signals.py
@@ -0,0 +1,39 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import *
+from .producer import publish
+from .serializers import *
+
+
+@receiver(post_save, sender=Item)
+def update_item(sender, instance, created, **kwargs):
+    """
+    Signal triggered when the Item object is updated
+    """
+    serializer = ItemSerializer(instance)
+
+    if not created:
+        publish('item_updated', serializer.data)
+
+
+@receiver(post_save, sender=OrderItem)
+def create_or_update_order_item(sender, instance, created, **kwargs):
+    """
+    Signal triggered when the OrderItem object is created or updated
+    """
+    serializer = OrderItemSerializer(instance)
+
+    if created:
+        publish('order_item_created', serializer.data)
+
+    if not created:
+        publish('order_item_updated', serializer.data)
+
+
+@receiver(post_delete, sender=OrderItem)
+def delete_order_item(sender, instance, **kwargs):
+    """
+    Signal triggered when the Item object is deleted
+    """
+    item_pk = instance.id
+    publish('order_item_deleted', item_pk)
