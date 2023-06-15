d4eb9466dc888f8640a4738e997367454001565b
repair-cat <simf61@yandex.ru>
2023-06-15 12:18:47 +0400

    employee model deleted

--- accounting/api/admin.py
+++ accounting/api/admin.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from .models import *
 
 
-@admin.register(Salary)
-class SalaryAdmin(admin.ModelAdmin):
-    list_display = ['employee_name', 'salary_per_month', 'taxes']
-
-
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['name', 'price']
