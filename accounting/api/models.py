2b1c27ea3dd1146efbd1a3c36804f9b7a919c23e
repair-cat <simf61@yandex.ru>
2023-06-15 12:22:17 +0400

    salary model was deleted

--- accounting/api/models.py
+++ accounting/api/models.py
@@ -1,26 +1,10 @@
 from django.db import models
 
 
-class Salary(models.Model):
-    employee_name = models.CharField(max_length=100)
-    salary_per_month = models.FloatField()
-    taxes = models.FloatField(default=0.00)
-
-    class Meta:
-        ordering = ('employee_name',)
-        db_table = 'salary'
-
-    def __str__(self):
-        return self.employee_name
-
-    def save(self, *args, **kwargs):
-        self.taxes = self.salary_per_month * 0.1
-        super(Salary, self).save(*args, **kwargs)
-
-
 class Item(models.Model):
     name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    qty = models.PositiveIntegerField(default=0)
 
     class Meta:
         ordering = ('name',)
@@ -33,7 +17,7 @@ class Item(models.Model):
 class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.SET_NULL, null=True)
     qty = models.PositiveIntegerField(default=0)
-    ordered_at = models.DateTimeField(verbose_name="Item was ordered")
+    ordered_at = models.DateTimeField()
     total_sum = models.FloatField(default=0.00)
 
     class Meta:
