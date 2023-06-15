05f2350f188a9a67df18711cbbfe0f250577c8fb
repair-cat <simf61@yandex.ru>
2023-06-15 15:44:31 +0400

    new messages added

--- sales/api/signals.py
+++ sales/api/signals.py
@@ -6,7 +6,7 @@ from .serializers import *
 
 
 @receiver(post_save, sender=Item)
-def update_item(sender, instance, created, **kwargs):
+def create_or_update_item(sender, instance, created, **kwargs):
     """
     Signal triggered when the Item object is updated
     """
@@ -15,6 +15,18 @@ def update_item(sender, instance, created, **kwargs):
     if not created:
         publish('item_updated', serializer.data)
 
+    elif created:
+        publish('item_created', serializer.data)
+
+
+@receiver(post_delete, sender=Item)
+def delete_item(sender, instance, **kwargs):
+    """
+    Signal triggered when the Item object is deleted
+    """
+    item_pk = instance.id
+    publish('item_deleted', item_pk)
+
 
 @receiver(post_save, sender=OrderItem)
 def create_or_update_order_item(sender, instance, created, **kwargs):
@@ -22,6 +34,7 @@ def create_or_update_order_item(sender, instance, created, **kwargs):
     Signal triggered when the OrderItem object is created or updated
     """
     serializer = OrderItemSerializer(instance)
+    serializer.data['ordered_at'] = instance.order.updated_at
 
     if created:
         publish('order_item_created', serializer.data)
