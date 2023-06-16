9554dbc4791ed1d2a2af0a5ca112ca6d2ac44f53
repair-cat <simf61@yandex.ru>
2023-06-16 19:03:57 +0400

    processing messages funcs changed

--- sales/api/signals.py
+++ sales/api/signals.py
@@ -1,22 +1,23 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import *
-from .producer import publish
+from .producer import publish_to_warehouse, publish_to_accounting
 from .serializers import *
 
 
 @receiver(post_save, sender=Item)
 def create_or_update_item(sender, instance, created, **kwargs):
     """
-    Signal triggered when the Item object is updated
+    Signal triggered when the Item object is created or updated
     """
     serializer = ItemSerializer(instance)
 
     if not created:
-        publish('item_updated', serializer.data)
+        publish_to_warehouse('item_updated', serializer.data)
+        publish_to_accounting('item_updated', serializer.data)
 
     elif created:
-        publish('item_created', serializer.data)
+        publish_to_accounting('item_created', serializer.data)
 
 
 @receiver(post_delete, sender=Item)
@@ -25,7 +26,7 @@ def delete_item(sender, instance, **kwargs):
     Signal triggered when the Item object is deleted
     """
     item_pk = instance.id
-    publish('item_deleted', item_pk)
+    publish_to_accounting('item_deleted', item_pk)
 
 
 @receiver(post_save, sender=OrderItem)
@@ -34,19 +35,18 @@ def create_or_update_order_item(sender, instance, created, **kwargs):
     Signal triggered when the OrderItem object is created or updated
     """
     serializer = OrderItemSerializer(instance)
-    serializer.data['ordered_at'] = instance.order.updated_at
 
     if created:
-        publish('order_item_created', serializer.data)
+        publish_to_accounting('order_item_created', serializer.data)
 
     if not created:
-        publish('order_item_updated', serializer.data)
+        publish_to_accounting('order_item_updated', serializer.data)
 
 
 @receiver(post_delete, sender=OrderItem)
 def delete_order_item(sender, instance, **kwargs):
     """
-    Signal triggered when the Item object is deleted
+    Signal triggered when the OrderItem object is deleted
     """
     item_pk = instance.id
-    publish('order_item_deleted', item_pk)
+    publish_to_accounting('order_item_deleted', item_pk)
