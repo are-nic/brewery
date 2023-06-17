9ecd2945fd42f8684c867a4a21ec7aad15af1557
repair-cat <simf61@yandex.ru>
2023-06-17 21:47:06 +0400

    tests

---
+++ sales/api/tests/test_api_items.py
@@ -0,0 +1,46 @@
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+from django.urls import reverse
+from api.models import Item
+from api.serializers import ItemSerializer
+
+
+User = get_user_model()
+
+
+class ItemsApiTestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_user(username='test', password='test')
+
+    def setUp(self):
+        """
+        Get user token and add it to headers. Create some items
+        """
+        self.client = APIClient()
+        url_token_auth = reverse('token')
+        request_data = {"username": "test", "password": "test"}
+        response = self.client.post(url_token_auth, request_data, format='json')
+        token = response.data.get('token')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+
+        self.item_1 = Item.objects.create(name='Test Dark Beer', price='20.00', qty=100)
+        self.item_2 = Item.objects.create(name='Test Sider', price='30.00', qty=1000)
+        self.item_3 = Item.objects.create(name='Test Vodka', price='10.00', qty=2000)
+
+    def test_get_items(self):
+        """ create some items and get list of them"""
+        url = reverse('items-list')
+        response = self.client.get(url)
+        serializer_data = ItemSerializer([self.item_1, self.item_2, self.item_3], many=True).data
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(response.data, serializer_data)
+
+    def test_get_detail_item(self):
+        """ Get item's details"""
+        url = reverse('items-detail', args=[self.item_1.id])
+        response = self.client.get(url)
+        serializer_data = ItemSerializer(self.item_1).data
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(response.data, serializer_data)
