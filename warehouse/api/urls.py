e299ba8be0abc782f1a4019a11f30a75d72c3489
repair-cat <simf61@yandex.ru>
2023-06-17 12:57:43 +0400

    name for url added

--- warehouse/api/urls.py
+++ warehouse/api/urls.py
@@ -7,6 +7,6 @@ router = SimpleRouter(trailing_slash=False)
 router.register('items', ItemViewSet, basename='items')
 
 urlpatterns = [
-    path('token-auth/', views.obtain_auth_token)
+    path('token-auth/', views.obtain_auth_token, name='token')
 ]
 urlpatterns += router.urls
