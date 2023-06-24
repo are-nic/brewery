41d708b3d25d961f4adb97a0d72589113e09cc2e
repair-cat <simf61@yandex.ru>
2023-06-24 14:11:02 +0400

    nginx

--- accounting/accounting/urls.py
+++ accounting/accounting/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('accounting/admin/', admin.site.urls),
     path('accounting/api/v1/', include('api.urls')),
 
-    re_path(r'^accounting/$^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^accounting/$^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^accounting/$^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'accounting/^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'accounting/^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'accounting/^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
