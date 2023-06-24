52925551f2a743775cfa5c83b02de6ad041b9f67
repair-cat <simf61@yandex.ru>
2023-06-24 14:45:36 +0400

    urls

--- warehouse/warehouse/urls.py
+++ warehouse/warehouse/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('warehouse/admin/', admin.site.urls),
     path('warehouse/api/v1/', include('api.urls')),
 
-    re_path(r'warehouse/^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'warehouse/^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'warehouse/^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^warehouse/swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^warehouse/swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^warehouse/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
